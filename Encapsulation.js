41e7ff8937530ca38e335a2cd90e445bb1591159
Sultan Mahmud <smrsaimon960@gmail.com>
2021-04-28 13:07:28 +0600

    encapsulation

---
+++ Encapsulation.js
@@ -0,0 +1,39 @@
+// Encapsulation 
+class User {
+
+  #_username
+  #_email
+
+  get username() {
+    return this.#_username
+  }
+
+  get email() {
+    return this.#_email
+  }
+
+
+  set username(newUsername) {
+    if (newUsername && newUsername.length <= 3) {
+      throw new Error('username must contain more than 3 characters.')
+    }
+
+    this.#_username = newUsername
+  }
+
+
+  set email(newEmail) {
+    if (newEmail && newEmail.length === 0) {
+      throw new Error('email must contain more than 0 characters.')
+    }
+
+    this.#_email = newEmail
+  }
+}
+
+const user1 = new User()
+
+
+user1.username = "fohjrdfjk";
+console.log(user1.username)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"# OOP-Core-concept" 
